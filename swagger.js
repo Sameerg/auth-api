5879a665b2636880e815255da8951c73cd858938
Sameer Gogia <sameer.gogia@bench.co>
2025-03-02 13:02:06 -0800

    updated route source for swagger

--- swagger.js
+++ swagger.js
@@ -9,7 +9,7 @@ const doc = {
 };
 
 const outputFile = './swagger-output.json';
-const routes = ['./src/routes/authRoutes.ts'];
+const routes = ['./src/app.ts'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
